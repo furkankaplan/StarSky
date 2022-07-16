2d96a0da3e4ef1616350e672eb378dbcc5112131
Furkan <furkankaplan@outlook.com>
2022-07-16 12:26:52 +0300

    Add StarProcuder to implement business logic

---
+++ StarSky/Domain/StarProducer.h
@@ -0,0 +1,12 @@
+//
+//  StarProducer.h
+//  StarSky
+//
+//  Created by Furkan Kaplan on 16.07.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface StarProducer : NSObject
+
+@end
---
+++ StarSky/Domain/StarProducer.m
@@ -0,0 +1,13 @@
+//
+//  StarProducer.m
+//  StarSky
+//
+//  Created by Furkan Kaplan on 16.07.2022.
+//
+
+#import "StarProducer.h"
+
+@implementation StarProducer
+
+
+@end
