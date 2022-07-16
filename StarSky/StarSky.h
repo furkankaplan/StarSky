0b33c07f43741280f2ff020d0e3fb07ee24601b5
Furkan <furkankaplan@outlook.com>
2022-07-16 10:56:38 +0300

    Initial project setup

---
+++ StarSky/StarSky.h
@@ -0,0 +1,18 @@
+//
+//  StarSky.h
+//  StarSky
+//
+//  Created by Furkan Kaplan on 16.07.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for StarSky.
+FOUNDATION_EXPORT double StarSkyVersionNumber;
+
+//! Project version string for StarSky.
+FOUNDATION_EXPORT const unsigned char StarSkyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StarSky/PublicHeader.h>
+
+
